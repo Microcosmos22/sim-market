320333e0a5c378e35f5a6e60acc0c49cdd45cebb
Andres <andres.husterzapke@stud.uni-goettingen.de>
2025-03-16 10:21:00 +0100

    FINAL: Website v2 up and running

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://v2202501113287307394.goodsrv.de:5050";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 console.log("API Base url ", API_BASE_URL);
 
 const fetchMachines = async () => {
